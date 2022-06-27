3a5cd0bca89add50c0413402318aa9d24501692b
lgq <lgq7000@163.com>
2022-06-27 20:47:56 +0800

    day

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ print(__name__)
 if __name__ == "__main__":
     print("hello")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model',default="resnet50",choices=['resnet50','dilated_conv'],
-                             help = 'resnet is ok'   ) 
+    parser.add_argument('--model',default="resnet50",choices=['resnet50','dilated_conv'],help = 'resnet is ok'   ) 
     parser.add_argument('--dataset',default="ECG200",help="200 is ok")
-    parser.add_argument('--batch_size',default=125)
-    parser.add_argument("--lr",default=0.01)
-    parser.add_argument('--epoch',default=50)
+    parser.add_argument('--batch_size',type=int,default=125)
+    parser.add_argument("--lr",type=float,default=0.01)
+    parser.add_argument('--epoch',type=int,default=50)
 
     args = parser.parse_args()
     print(args)
-    print(args.lr)
+    print(type(args.lr))
 
     
--- models/dilated_conv.py
+++ models/dilated_conv.py
@@ -11,54 +11,51 @@ import torch.nn.functional as F
 import numpy as np
 
 
-
-class SamePadConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, groups=1):
+class SameShapeConv(nn.Module):
+    '''
+        conv but  shape dont change 
+    '''
+    def __init__(self,in_channels,out_channels,kernel_size,dilation=1):
         super().__init__()
-        self.receptive_field = (kernel_size - 1) * dilation + 1
-        padding = self.receptive_field // 2
-        self.conv = nn.Conv1d(
-            in_channels, out_channels, kernel_size,
-            padding=padding,
-            dilation=dilation,
-            groups=groups
-        )
-        self.remove = 1 if self.receptive_field % 2 == 0 else 0
+        self.receptive_field = (kernel_size -1) * dilation + 1
+        padding = self.receptive_field //2
+        self.conv = nn.Conv1d(in_channels,out_channels,kernel_size,padding=padding,dilation=dilation)
         
-    def forward(self, x):
+    def forward(self,x):
         out = self.conv(x)
-        if self.remove > 0:
-            out = out[:, :, : -self.remove]
-        return out
-    
+        return out if self.receptive_field % 2 != 0 else out[:,:-1]
+        
+
 class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation, final=False):
-        super().__init__()
-        self.conv1 = SamePadConv(in_channels, out_channels, kernel_size, dilation=dilation)
-        self.conv2 = SamePadConv(out_channels, out_channels, kernel_size, dilation=dilation)
-        self.projector = nn.Conv1d(in_channels, out_channels, 1) if in_channels != out_channels or final else None
-    
-    def forward(self, x):
-        residual = x if self.projector is None else self.projector(x)
-        x = F.gelu(x)
-        x = self.conv1(x)
-        x = F.gelu(x)
-        x = self.conv2(x)
-        return x + residual
+        '''conv block:
+            two sameshapeconv
+            with residual
+        '''
+        def __init__(self,in_channels,out_channels,kernel_size,dilation=1):
+            super().__init__()
+            self.conv1 = SameShapeConv(in_channels,out_channels,kernel_size,dilation=dilation)
+            self.conv2 = SameShapeConv(out_channels,out_channels,kernel_size,dilation=dilation)
+            self.projector = nn.Conv1d(in_channels,out_channels,1)  if in_channels != out_channels else None
+
+        def forward(self,x):
+            residual = x if self.projector is None else self.projector(x)
+            out = F.gelu(x)
+            out = self.conv1(out)
+            out = F.gelu(out)
+            out = self.conv2(out)
+            return out + residual
+        
 
 class DilatedConvEncoder(nn.Module):
-    def __init__(self, in_channels, channels, kernel_size):
+    '''
+            
+    '''
+    def __init__(self,in_channels,channels,kernel_size): # channels: hiddle layer and output  channels list
         super().__init__()
-        self.net = nn.Sequential(*[
-            ConvBlock(
-                channels[i-1] if i > 0 else in_channels,
-                channels[i],
-                kernel_size=kernel_size,
-                dilation=2**i,    # 膨胀系数指数增大，符合迭代需求
-                final=(i == len(channels)-1)
-            )
-            for i in range(len(channels))
-        ])
+        self.net = nn.Sequential(
+                *[ ConvBlock(in_channels if i==0 else channels[i-1], channels[i], kernel_size, dilation=2**i) 
+                       for i in range(len(channels))]
+        )
         
-    def forward(self, x):
+    def forward(self,x):
         return self.net(x)
